f37be39964a8cb988a64df60258b6e2a63b1a97d
Joshua Ohm <joshua.ohm@gmail.com>
2018-01-04 12:15:30 -0800

    Added css and font changes
    added new font to header
    
    deleted comment from my-work

--- php/resources/views/includes/head.blade.php
+++ php/resources/views/includes/head.blade.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" href="css/normalize.css">
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/fontello.css">
-<link href="https://fonts.googleapis.com/css?family=Audiowide|Monoton|Press+Start+2P" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Audiowide|Monoton|Press+Start+2P|Russo+One" rel="stylesheet">
 <script
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
--- php/resources/views/pages/my-work.blade.php
+++ php/resources/views/pages/my-work.blade.php
@@ -1,51 +1,6 @@
 @extends('layout.default')
 @section('content')
 {{ Html::script('js/my-work.js') }}
-
-<!---
-<div class="my-work-container">
-	<div class="row">
-		<div class="top">
-			<div class="btn-wrapper">
-				<div class="btn left">prev</div>
-			</div>
-			@foreach ($works as $index => $work)
-					@if ($index == 0)
-						<a href="{{ $work->url }}" id="img-{{ $index }}"><img class="active" src='{{ asset("img/".$work->name."-big.jpg") }}'></a>
-					@else
-						<a href="{{ $work->url }}" id="img-{{ $index }}"><img src='{{ asset("img/".$work->name."-big.jpg") }}'></a>
-					@endif
-			@endforeach
-			<div class="btn-wrapper">
-				<div class="btn right">next</div>
-			</div>
-		</div>
-		<div class="bottom">
-			@foreach ($works as $index => $work)
-				@if ($index == 0)
-					<div class="details active" id="details-{{ $index }}">
-						@foreach ($details[$index] as $detail)
-							<div class="detail">{{ $detail }}</div>
-						@endforeach
-						<div class="btn-wrapper">
-							<a class="btn link" href='{{ $work->url }}'>View Site</a>
-						</div>
-					</div>
-				@else
-					<div class="details" id="details-{{ $index }}">
-						@foreach ($details[$index] as $detail)
-							<div class="detail">{{ $detail }}</div>
-						@endforeach
-						<div class="btn-wrapper">
-							<a class="btn link" href='{{ $work->url }}'>View Site</a>
-						</div>
-					</div>
-				@endif
-			@endforeach
-		</div>
-	</div>
-</div>
--->
 <div class="my-work-container">
     <div class="row">
         <div class="top">
