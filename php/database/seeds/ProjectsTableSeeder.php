f27fe914d7706288642239c07655c8a5339c06f8
root <root@johm.ubuntu>
2019-06-03 22:29:44 -0400

    Added status panel to projects table seeder. Added merchants craft images for real this time.

--- php/database/seeds/ProjectsTableSeeder.php
+++ php/database/seeds/ProjectsTableSeeder.php
@@ -20,6 +20,15 @@ class ProjectsTableSeeder extends Seeder
             'details' => "An app for your time spent working on tasks/projects. Built with React and Laravel.",
             'repo' => "https://github.com/joshuaohm/TimeTracker/" 
         ]);
+
+        DB::table('projects')->insert([
+            'id' => 2,
+            'url' => "https://github.com/joshuaohm/status-panel",
+            'title' => "Status Panel",
+            'name' => "status-panel",
+            'details' => "Client and Server apps that allow you to monitor device connections and send messages. Built with Websockets, Node, and a little Android",
+            'repo' => "https://github.com/joshuaohm/status-panel/" 
+        ]);
     }
 }
 
